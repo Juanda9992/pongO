d8d0bedd9f54fefa989cc2e7870c1c19334fcc53
Juanda9992 <juanda999games@gmail.com>
2022-09-01 12:29:50 -0500

    Version 0.1 Imported all the packages (DoTween, Photon, TMPro) Set up scene Added 2 Players Added Controls for Player 1 Added

---
+++ Assets/Player/Player_Control.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float minY,maxY;
+    [SerializeField] private float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void moveUp()
+    {
+        if(transform.position.y < maxY)
+        {    
+            rb.velocity = transform.up * speed;
+        }
+    }
+
+    public void moveDown()
+    {
+        if(transform.position.y > minY)
+        {
+            rb.velocity = -transform.up * speed;
+        }
+    }
+
+    public void StopMoving()
+    {
+        rb.velocity = Vector2.zero;
+    }
+
+}
