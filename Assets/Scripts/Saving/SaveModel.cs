367da9f1a8ddb9678f040ea30f6e2de1e1d6dabd
Juanda999 <juanda999games@gmail.com>
2023-08-25 21:32:43 -0500

    Create SaveModel for saving / loading the data

---
+++ Assets/Scripts/Saving/SaveModel.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SaveModel", menuName = "Save data/SaveModel", order = 0)]
+public class SaveModel : ScriptableObject 
+{
+    public int matchesWin;
+    public int matchesLoss;
+    public Vector3 color;
+
+    public List<bool> colorUnlocked;
+
+}
